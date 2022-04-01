9b2468d731e6b580a42c892ac9942b996b3b7499
aidanmelen <aidan-melen@protonmail.com>
2022-04-01 21:39:06 +0000

    lint

--- examples/basic/providers.tf
+++ examples/basic/providers.tf
@@ -22,4 +22,3 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.cluster.token
 }
-
