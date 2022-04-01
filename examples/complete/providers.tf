735d25b8191e00b813618d491d8835e886d007ee
Aidan Melen <aidan-melen@protonmail.com>
2022-04-01 13:11:53 -0600

    Update providers.tf

--- examples/complete/providers.tf
+++ examples/complete/providers.tf
@@ -10,6 +10,14 @@ provider "aws" {
   }
 }
 
+# used to patch the aws-auth configmap
+provider "kubectl" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
+
+# used to create resources on the kubernetes cluster
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
